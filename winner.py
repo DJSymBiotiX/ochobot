386ccb89f0fc9106b9ba71be67d3513ca0f2b082
Fernando Chorney <github@djsbx.com>
2015-08-19 16:42:25 -0500

    Got a search going

--- winner.py
+++ winner.py
@@ -11,83 +11,49 @@ from sys import exit
 def main():
     args = parse_args()
 
-    out("Checking For Ochos....")
-
-    limit = 1
-
-    #t = Twitter(args.configPath)
+    # t = Twitter API, p = PSQL Class (Not really used), s = PSQL Session
+    t = Twitter(args.configPath)
     p = PSQL(args.configPath)
-    session = p.session
+    s = p.session
 
-    x = session.execute('SELECT * FROM followers');
-    for row in x:
-        print row
+    contests = []
+
+    contests += scan_for_contests(t, s)
 
     exit(0)
+
+def scan_for_contests(twitter, session):
+    out("Scanning For Contests...")
+    last_twitter_id = session.execute(
+        "SELECT last_twitter_id FROM info"
+    ).fetchone()[0]
+    items = []
+
     try:
-        eight = t.api.GetSearch(
-            term='Eight',
-            count=10,
-            result_type='recent',
-            lang='en'
+        results = twitter.api.GetSearch(
+            term="RT to win",
+            since_id=last_twitter_id
         )
+
+        for status in results:
+            item = status.AsDict()
+            if item['retweet_count'] > 0:
+                if item['id'] > last_twitter_id:
+                        # Update last_twitter_id
+                        session.execute(
+                            "UPDATE info SET last_twitter_id = '%s'" % (
+                                item['id']
+                            )
+                        )
+                        session.commit()
+                        items.append(item)
+                        out(item)
     except Exception as e:
-        err("[Main] Search Failure: %s" % e)
-        eight = []
-        ocho = []
+        err("[scan_for_contests] Search Error: %s" % e)
+    return items
 
-    matches = []
-    for x in eight:
-        text = x.AsDict()['text']
-        blacklist = [
-            'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven',
-            'nine'
-        ]
-        if filter_search(text, 'eight', blacklist) is not None:
-            matches.append(x)
 
-    count = 0
-    for x in matches:
-        out(x.AsDict())
-        out('\n\n\n')
-        count += 1
-        if count == limit:
-            exit(0)
 
-    exit(0)
-
-def filter_search(text, term, blacklist):
-    idx = text.lower().find(term.lower())
-    if idx == -1:
-        return None
-
-    # Ignore retweets
-    if text.lower()[0:2] == 'rt':
-        return None
-
-    # Ignore if has blacklist text
-    for item in blacklist:
-        if item.lower() in text.lower():
-            return None
-
-    whitelist = [' ', '.', '-', '\n']
-    before = ''
-    after = ''
-
-    len8 = len(term)
-    if idx + len8 >= len(text):
-        after = ' '
-    else:
-        after = text[idx + len8]
-
-    if idx - 1 <= 0:
-        before = ' '
-    else:
-        before = text[idx - 1]
-
-    if before in whitelist and after in whitelist:
-        return "That's Ocho!"
-    return None
 
 def parse_args():
     import argparse
